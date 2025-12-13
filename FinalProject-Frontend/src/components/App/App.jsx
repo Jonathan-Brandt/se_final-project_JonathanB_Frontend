acba95dc1b12dd0c5e3c22431639add63bd9fbdf
Jonathan Brandt <jwilliambrandtwork@gmail.com>
2025-12-12 21:05:37 -0600

    Final Project: First frontend kickoff!

--- FinalProject-Frontend/src/components/App/App.jsx
+++ FinalProject-Frontend/src/components/App/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import Header from "../Header/Header";
+import MainPage from "../MainPage/MainPage";
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <div className="page">
         <div className="page__content">
           <Header></Header>
+          <MainPage></MainPage>
         </div>
       </div>
     </>
---
+++ FinalProject-Frontend/src/components/MainPage/MainPage.jsx
@@ -0,0 +1,27 @@
+import "./MainPage.css";
+
+function MainPage() {
+  return (
+    <>
+      <main className="main-page">
+        <h1 className="main-page__text">What's going on in the world?</h1>
+        <p className="main-page__subtext">
+          Find the latest new on any topic and save them to your personal
+          account
+        </p>
+        <div className="search-bar__container">
+          <form action="" className="search-bar">
+            <input
+              type="text"
+              className="search-bar__input"
+              placeholder="Enter topic"
+            />
+            <button className="search-button">Search</button>
+          </form>
+        </div>
+      </main>
+    </>
+  );
+}
+
+export default MainPage;
