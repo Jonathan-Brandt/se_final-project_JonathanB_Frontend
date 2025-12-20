23db993cba795858ac1b5fdc62430c6e71294d9f
Jonathan Brandt <jwilliambrandtwork@gmail.com>
2025-12-19 20:49:34 -0600

    Final Project Frontend: Basic card markup is done :)

--- FinalProject-Frontend/src/components/NewsCardList/NewsCardList.jsx
+++ FinalProject-Frontend/src/components/NewsCardList/NewsCardList.jsx
@@ -1,10 +1,14 @@
 import "./NewsCardList.css";
+import NewsCard from "../NewsCard/NewsCard";
 
 function NewsCardList() {
   return (
     <>
-      <div className="newscardlist">
+      <div className="newscardlist-container">
         <h1 className="newscardlist__header">Search results</h1>
+        <ul className="newscardlist">
+          <NewsCard></NewsCard>
+        </ul>
         <button className="showmore__button" type="button">
           Show more
         </button>
